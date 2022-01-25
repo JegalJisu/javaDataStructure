83d33dbf5eb4a38b962aa51bff72f112bed74cf5
JegalJisu <rkfwltn12@naver.com>
2022-01-25 18:02:33 +0900

    14일차
    1. Collections List 이해 및 문제 풀이

---
+++ src/bjpublic/chap13/IntArrayListExample.java
@@ -0,0 +1,18 @@
+package bjpublic.chap13;
+
+public class IntArrayListExample {
+	private int[] array = new int[10];
+	private int index = 0;
+	
+	public void add(int value) {
+		this.array[index++] = value;
+	}
+	
+	public int getValue(int index) {
+		return this.array[index];
+	}
+	
+	public int size() {
+		return this.array.length;
+	}
+}
